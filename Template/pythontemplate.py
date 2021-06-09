d90dd5bd349c9e808eca7e674a58403a1a16aae9
Mukesh Bahuguna <ud145544@gmail.com>
2021-06-09 12:08:23 +0530

    added templates

--- Template/pythontemplate.py
+++ Template/pythontemplate.py
@@ -45,5 +45,55 @@ mod=1000000007
 
 print(stdint())
 ##############################
+
+#######third template##############
+PI = 3.141592653589793238460
+INF =  999999999
+MOD  = 1000000007
+# MOD = 998244353
+
+def add(x,y):
+    return (x+y)%MOD
+
+def sub(x,y):
+    return (x-y+MOD)%MOD
+
+def mul(x,y):
+    return (x*y)%MOD
+
+def gcd(x,y):
+    if y == 0:
+        return x
+    return gcd(y,x%y)
+
+def lcm(x,y):
+    return (x*y)//gcd(x,y)
+
+def power(x,y):
+    res = 1
+    x%=MOD
+    while y!=0:
+        if y&1 :
+            res = mul(res,x)
+        y>>=1
+        x = mul(x,x)
+        
+    return res
+        
+def mod_inv(n):
+    return power(n,MOD-2)
+
+def prob(p,q):
+    return mul(p,power(q,MOD-2))    
+  
+def ii():
+    return int(input())
+
+def li():
+    return [int(i) for i in input().split()]
+
+def ls():
+    return [i for i in input().split()]
+################
 # if __name__=='__main__':
 #code
