de37e07c4a3abbd64512a9b7bd3977bb23815c12
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2022-09-16 19:30:23 +0530

    Time: 503 ms (15.13%), Space: 137.6 MB (44.51%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,36 +1,19 @@
 class Solution {
-    public int[] findOriginalArray(int[] c) {
-        int n= c.length;
-        if(n%2!=0) return new int[0];
-        
-        int[] a=new int[n/2]; 
-        Arrays.sort(c); 
-        Map<Integer,Integer> m =new HashMap<>();
-        
-        for (int i:c)
-            m.put(i , m.getOrDefault(i,0)+1);
-        
-        int j=0;
-        for (int i=0; i<n ;i++){
-            int x= c[i]*2;
-            if(c[i]!=0){
-                if (m.containsKey(x) && m.get(c[i])>0 && m.get(x)>0){
-                    a[j++]=c[i];
-                    m.put(c[i], m.get(c[i])-1);
-                    m.put(x, m.get(x)-1);
-                }
-            }
-            else{
-                if(m.get(c[i])<=1) continue; 
-                else {
-                    a[j++]=c[i];
-                    m.put(c[i], m.get(c[i])-1);
-                    m.put(x, m.get(x)-1);
-                }
+    public int[] findOriginalArray(int[] A) {
+        int n = A.length, i = 0;
+        if (n % 2 == 1) return new int[0];
+        int[] res = new int[n / 2];
+        Map<Integer, Integer> count = new TreeMap<>();
+        for (int a : A)
+            count.put(a, count.getOrDefault(a, 0) + 1);
+        for (int x : count.keySet()) {
+            if (count.get(x) > count.getOrDefault(x + x, 0))
+                return new int[0];
+            for (int j = 0; j < count.get(x); ++j) {
+                res[i++] = x;
+                count.put(x + x, count.get(x + x) - 1);
             }
         }
-        // System.out.println(Arrays.toString(a)+j);
-        return j==n/2 ? a : new int[0];
-        
+        return res;
     }
 }
