e646715e0b3ecfea5a702e05516ad18d5c928d72
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2023-07-24 13:40:52 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (75.61%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,18 @@
 class Solution {
-    public double myPow(double x, int n) {
-        return Math.pow(x,n);
+     public double myPow(double x, int n) {
+        return helper(x, n);
+    }
+    
+    private double helper(double x, int n) {
+        if (x == 0) return 0;
+        if (n == 0) return 1;
+        
+        double res = helper(x, n / 2);
+        res *= res;
+        if (n % 2 != 0) {
+            return (n > 0) ? res * x : res / x;
+        } else {
+            return res;
+        }
     }
 }
