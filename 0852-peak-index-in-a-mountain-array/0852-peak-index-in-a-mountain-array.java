8f8756058a6ca37b9ca7d00a429b64726c089844
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2023-07-25 15:49:33 +0530

    Time: 0 ms (100.00%), Space: 55.6 MB (41.53%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -5,16 +5,9 @@ class Solution {
         
         while(i<=j){
             int mid = i + (j-i)/2;
-            if(mid==0) return (a[mid] > a[mid+1] ? mid :mid+1);
-            
-            else if(mid==n-1) return a[mid] > a[mid-1] ? mid :mid-1;
-            
-            else{
-                if(a[mid]> a[mid+1] && a[mid]>a[mid-1]) return mid;
-                else if(a[mid+1]>a[mid-1]) i=mid+1;
-                else j=mid-1;
-            }
-            
+            if((mid==0 && a[mid] > a[mid+1]) || (mid==n-1 && a[mid] > a[mid-1] )) return mid ;
+            else if(a[mid]<a[mid+1]) i=mid+1;
+            else j=mid-1;
         }
         return i;
     }
