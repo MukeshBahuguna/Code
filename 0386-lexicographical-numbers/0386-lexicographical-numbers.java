4549e8fed66f56767a719625e405ec5f9986fb8b
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2023-08-07 11:36:50 +0530

    Time: 1 ms (100.00%), Space: 47.6 MB (44.81%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,17 +1,17 @@
 class Solution {
     List<Integer> l = new ArrayList<>();
     public List<Integer> lexicalOrder(int n) {
-        f(n,1,10, 1);
+        f(n,1,true, 1);
         return l;
     }
-    public void f(int n, int i ,int c ,int val) {
+    public void f(int n, int i ,boolean flag ,int val) {
         if(val>n) return ;
         if(l.size()>=n) return;
-        if(val%10==0 && c==0) return ;
+        if(val%10==0 && !flag) return ;
         
         l.add(val);
-        f(n ,i, 1, val*10);
-        f(n,i+1 ,0, val+1);
+        f(n ,i, true, val*10);
+        f(n,i+1 ,false, val+1);
     }
     
     
