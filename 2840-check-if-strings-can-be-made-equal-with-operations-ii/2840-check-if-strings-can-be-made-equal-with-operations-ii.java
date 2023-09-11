127df55392351523a1e02e2bb12d9a8f756d96c7
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2023-09-11 18:14:59 +0530

    Time: 31 ms (47.63%), Space: 45.1 MB (34.94%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -4,17 +4,14 @@ class Solution {
         Map<Character , int[]> m = new HashMap<>();
         for(int i=0 ; i<n ;i++){
             char ch = s1.charAt(i);
+            if(m.containsKey(ch)) m.get(ch)[i%2]+=1;
             
-            if(m.containsKey(ch)){
-                m.get(ch)[i%2]+=1;
-            }
             else{
                 int[] arr =new int[2];
                 arr[i%2]+=1;
                 m.put(ch , arr);
             }
         }
-         
         for(int i=0 ; i<n ;i++){
             char ch=s2.charAt(i);
             if(m.containsKey(ch) &&  m.get(ch)[i%2]>0) m.get(ch)[i%2]-=1;
