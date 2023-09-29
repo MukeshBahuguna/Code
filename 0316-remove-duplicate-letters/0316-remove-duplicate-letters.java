7d82ce88dda883e1d6b86c02e7cde63fc4710398
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2023-09-29 18:15:39 +0530

    Time: 2 ms (96.18%), Space: 41.2 MB (53.30%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,32 +1,26 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        Stack<Character> stack= new Stack<>();
-        
-        int[] a = new int[26];        
-        int n=s.length() , c=0;
-
-        String ans="";
-        for(int i=0; i<n ;i++){
-            char ch =s.charAt(i);
-            a[ch-97]+=1;   
-        }
-        int[] arr= new int[26];
-        for(int i=0; i<n ;i++){
-            char ch =s.charAt(i);
-            if(arr[ch-97]!=1){
-                while(!stack.isEmpty() && stack.peek()>=ch && (a[stack.peek()-97]-1)>0 ){
-                    a[stack.peek()-97]-=1;
-                    arr[stack.peek()-97]=0;
-                    stack.pop();
-
-                }
-                stack.push(ch);
-                arr[ch-97]=1;
-            } 
-            else a[ch-97]-=1;
+        int[] lastIndex = new int[26];
+        for (int i = 0; i < s.length(); i++){
+            lastIndex[s.charAt(i) - 'a'] = i; 
         }
         
-        while(!stack.isEmpty()) ans=stack.pop()+ans;
-        return ans;
+        boolean[] seen = new boolean[26]; 
+        Stack<Integer> st = new Stack();
+        
+        for (int i = 0; i < s.length(); i++) {
+            int curr = s.charAt(i) - 'a';
+            if (seen[curr]) continue; 
+            while (!st.isEmpty() && st.peek() > curr && i < lastIndex[st.peek()]){
+                seen[st.pop()] = false; 
+            }
+            st.push(curr);
+            seen[curr] = true; 
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty())
+            sb.append((char) (st.pop() + 'a'));
+        return sb.reverse().toString();
     }
 }
