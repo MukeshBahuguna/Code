7f0db59e8cb22ea323261e26ab118402bd181801
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2023-10-03 23:41:01 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (73.19%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -3,9 +3,7 @@ class Solution {
         int ans=0,n=nums.length;
         int[] a = new int[101];
         
-        for(int i=0  ;i<n ;i++) a[nums[i]]+=1;
-        
-        for(int i=0  ;i<101 ;i++) ans+=(a[i] * (a[i]-1))/2;
+        for(int i=0  ;i<n ;i++) ans+=a[nums[i]]++;
         return ans;
     }
 }
