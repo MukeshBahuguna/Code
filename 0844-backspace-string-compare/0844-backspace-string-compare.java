15025e9a2455f61dadedbd3473159228654fff4b
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2023-10-19 11:52:25 +0530

    Time: 6 ms (8.21%), Space: 40.4 MB (67.85%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,7 +1,11 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
+        String s1=strCmp(s);
+        String s2=strCmp(t);
+        return s1.equals(s2);
+    }
+    public String strCmp(String s){
         String s1="";
-        String s2="";
         int c=0;
         for(int i=s.length()-1 ; i>=0 ;i--){
             if(s.charAt(i)=='#') c+=1;
@@ -10,17 +14,7 @@ class Solution {
                 if(c==0) s1+= s.charAt(i) ;
                 else c-=1;
             }
-            
         }
-        c=0;
-        for(int i=t.length()-1 ; i>=0 ;i--){
-            if(t.charAt(i)=='#') c+=1;
-            
-            else{
-                if(c==0) s2+= t.charAt(i) ;
-                else c-=1;
-            }
-        }
-        return s1.equals(s2);
+        return s1;
     }
 }
