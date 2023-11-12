9fbf5fdb5d6e608e6e2ed3d27ef49c100c5731ff
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2023-11-12 11:28:04 +0530

    Time: 4 ms (25.00%), Space: 42.8 MB (100.00%) - LeetHub

--- 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.java
+++ 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.java
@@ -3,12 +3,15 @@ class Solution {
         //range nums[i]+nums[i]
         //2p & 2p-1 xor
         int n= nums.length;
+        Arrays.sort(nums);
+
         int ans=0;
         for(int i=0 ;i<n ;i++){
-            for(int j=0 ; j<n ;j++){
+            for(int j=i ; j<n ;j++){
                 if(Math.min(nums[i] , nums[j]) >= Math.abs(nums[i] - nums[j])){
-                    ans=Math.max(ans, nums[i]^nums[j]);
+                    ans=Math.max(ans,nums[i]^nums[j]);
                 }
+                else break;
             }
         }
         return ans;
