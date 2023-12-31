a29e7c1d3a8d273072a79c8db8baafc6501e383d
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2023-12-31 12:52:11 +0530

    Time: 3 ms (37.34%), Space: 41.4 MB (13.28%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
@@ -3,12 +3,8 @@ class Solution {
         Map<Character,Integer> m =new HashMap<>();
         int max=-1;
         for(int i=0 ; i<s.length() ;i++){
-            if (m.containsKey(s.charAt(i))){
-                max=Math.max(max , i-m.get(s.charAt(i))-1);
-            }
-            else{
-                m.put(s.charAt(i) , i);
-            }
+            if (m.containsKey(s.charAt(i))) max=Math.max(max , i-m.get(s.charAt(i))-1);
+            else m.put(s.charAt(i) , i);
         }
         return max;
     }
