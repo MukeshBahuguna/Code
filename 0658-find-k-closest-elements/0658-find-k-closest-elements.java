e1d82ccba030c5dddc4f4e50377a557ce0920ad4
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2024-04-09 10:46:02 +0530

    Time: 48 ms (9.46%), Space: 45.8 MB (48.58%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -3,42 +3,30 @@ class Solution {
         List<Integer> l= new ArrayList<>();
         int n=arr.length;
         
-        int i=0 ,j=n-1;
-        int ans=-1;
-        while(i<=j){
-            int mid= i+((j-i)/2);
-            if(arr[mid]<=x){
-                i=mid+1;
-            }
-            else j=mid-1;
-        }
-        j= j>=0 ? j:0;
-        if(j+1<n && Math.abs(arr[j+1] - x) < Math.abs(arr[j] - x )) j=j+1;
+        PriorityQueue<Node> pq =new PriorityQueue<>((a,b)-> b.diff == a.diff ? b.idx -a.idx  : b.diff - a.diff);
         
-        l.add(arr[j]);
-        
-        int forwd=j+1 , backwd=j-1;
-        while(forwd<n && backwd>=0  && l.size()<k){
-            if (Math.abs(arr[forwd] - x ) < Math.abs(arr[backwd] - x )) {
-                l.add(arr[forwd]);
-                forwd+=1;
-            }
-            else{
-                l.add(arr[backwd]);
-                backwd-=1;
-            }
+        for(int i=0 ; i<n ;i++){
+            Node node=new Node(i ,arr[i], Math.abs(arr[i]-x));
+            pq.add(node);
+            
+            if(pq.size()>k) pq.poll();
         }
-        while(forwd<n && l.size()<k){
-            l.add(arr[forwd]);
-            forwd+=1;
-        }
-        
-        while(backwd>=0  && l.size()<k){
-            l.add(arr[backwd]);
-            backwd-=1;
-        }
-               
+        while(!pq.isEmpty()) l.add(pq.poll().ele);
         Collections.sort(l);
         return l;
+        
+    }
+}
+
+
+class Node {
+    int idx;
+    int ele;
+    int diff;
+    
+    Node(int idx , int ele , int diff){
+        this.idx =idx;
+        this.ele =ele;
+        this.diff =diff;       
     }
 }
