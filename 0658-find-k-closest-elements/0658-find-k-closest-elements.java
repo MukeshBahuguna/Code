72c55695ca481dea9eac2a3bc95ddd0e1d86eee9
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2024-04-09 11:01:14 +0530

    Time: 5 ms (59.54%), Space: 46.5 MB (7.63%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,32 +1,13 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> l= new ArrayList<>();
-        int n=arr.length;
-        
-        PriorityQueue<Node> pq =new PriorityQueue<>((a,b)-> b.diff == a.diff ? b.idx -a.idx  : b.diff - a.diff);
-        
-        for(int i=0 ; i<n ;i++){
-            Node node=new Node(i ,arr[i], Math.abs(arr[i]-x));
-            pq.add(node);
-            
-            if(pq.size()>k) pq.poll();
+        int left = 0, right = arr.length - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - arr[mid] > arr[mid + k] - x)
+                left = mid + 1;
+            else
+                right = mid;
         }
-        while(!pq.isEmpty()) l.add(pq.poll().ele);
-        Collections.sort(l);
-        return l;
-        
-    }
-}
-
-
-class Node {
-    int idx;
-    int ele;
-    int diff;
-    
-    Node(int idx , int ele , int diff){
-        this.idx =idx;
-        this.ele =ele;
-        this.diff =diff;       
+        return Arrays.stream(arr, left, left + k).boxed().collect(Collectors.toList());
     }
 }
