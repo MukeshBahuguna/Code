4f735c5e2be1b6430a30f2e092939c416ef1d386
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2024-04-28 17:19:37 +0530

    Time: 31 ms (55.63%), Space: 50.3 MB (83.55%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,34 +1,15 @@
 class Solution {
-    
-    class Pair{
-        double val;
-        int[] cord;
-        
-        public Pair(double val , int[] cord){
-            this.val=val;
-            this.cord=cord;
+    public int[][] kClosest(int[][] points, int K) {
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+    for (int[] p : points) {
+        pq.offer(p);
+        if (pq.size() > K) {
+            pq.poll();
         }
     }
+    int[][] res = new int[K][2];
+    while (K > 0)  res[--K] = pq.poll();
     
-    public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Pair> pq =new PriorityQueue<Pair>((a,b)->b.val>a.val ? 1 : b.val==a.val ? 0: -1);
-        
-        int n=points.length;
-        for (int i=0 ; i<n ;i++){
-            pq.add(new Pair(findDist(points[i][0],points[i][1]) , points[i]));
-            if(pq.size()>k){
-                pq.poll();
-            }
-        }
-        int i=0;
-        int[][] ans= new int[k][2];
-        while(!pq.isEmpty()){
-            Pair p= pq.poll();
-            ans[i++]=p.cord;
-        }
-        return ans;
-    }
-    public double findDist(int x ,int y){
-        return Math.sqrt(x*x+y*y);
+    return res;
     }
 }
