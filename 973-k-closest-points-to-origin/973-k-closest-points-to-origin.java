d94d7b0101b0d0a578dcee1e8df9ae90b183dcd6
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2024-04-28 17:19:53 +0530

    Time: 31 ms (55.63%), Space: 50.5 MB (78.18%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -3,9 +3,8 @@ class Solution {
         PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
     for (int[] p : points) {
         pq.offer(p);
-        if (pq.size() > K) {
-            pq.poll();
-        }
+        if (pq.size() > K) pq.poll();
+        
     }
     int[][] res = new int[K][2];
     while (K > 0)  res[--K] = pq.poll();
