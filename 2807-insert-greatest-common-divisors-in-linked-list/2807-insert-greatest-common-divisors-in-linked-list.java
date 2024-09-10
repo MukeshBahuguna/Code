f1ee1afef9bcee256f27d1d8d6b91e7c80cb7e26
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2024-09-10 11:24:21 +0530

    Time: 2 ms (71.11%), Space: 45 MB (61.64%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.java
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.java
@@ -24,7 +24,6 @@ class Solution {
     public int gcd(int x , int y){
         if(x<y) return gcd(y,x);
         if(y==0) return x;
-        else if(y==1) return 1;
         return gcd(y, x%y);
     }
 }
