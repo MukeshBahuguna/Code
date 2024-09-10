e5efa93470a716e962cfa46937de2f7635d6f713
Mukesh Bahuguna <52329826+MukeshBahuguna@users.noreply.github.com>
2024-09-10 11:00:54 +0530

    Time: 2 ms (75.67%), Space: 46.2 MB (70.13%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,28 +1,23 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        int n= graph.length;
-        
         List<List<Integer>> l = new ArrayList();
         List<Integer> x = new ArrayList();
-        
-        dfsHelper(graph, 0, n , l,x);
+        x.add(0);
+        dfsHelper(graph, 0 , l,x);
         return l;
     }
     
-    public void dfsHelper(int[][] graph, int s, int n, List<List<Integer>> l ,List<Integer> x ){
-        x.add(s);
+    public void dfsHelper(int[][] graph, int s, List<List<Integer>> l ,List<Integer> x ){
+
+        if(s==graph.length-1){
+            l.add(new ArrayList(x));
+            return;
+        }
         
-        for(int j=0 ; j< graph[s].length ; j++){
-            int ele= graph[s][j];
-            if(ele==n-1){
-                x.add(ele);
-                l.add(new ArrayList(x));   
-                x.remove(x.size()-1);
-            }
-            else  {
-                dfsHelper(graph , ele , n , l , x);
-                x.remove(x.size()-1);
-            }
+        for(int j : graph[s]){
+            x.add(j);
+            dfsHelper(graph , j ,l , x);
+            x.remove(x.size()-1);
         }
     }
 }
